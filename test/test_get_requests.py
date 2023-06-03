c2df68d25c97a802f4d6320c113595b077b7f642
absiddik7 <abubakkar1070@gmail.com>
2023-06-03 17:51:46 +0600

    first commit

--- test/test_get_requests.py
+++ test/test_get_requests.py
@@ -3,14 +3,15 @@ import requests
 import jsonpath
 
 
-def test_get_request():
+def test_get_list_users():
     url = "https://reqres.in/api/users?page=2"
     response = requests.get(url)
+    assert response.status_code == 200, "Response code is not 200. The response code is " + str(response.status_code)
 
     # parse response to json
     json_object = json.loads(response.content)
     json_formatted_response = json.dumps(json_object, indent=2)
-    #print(json_formatted_response)
+    print(json_formatted_response)
 
     # fetch value from the response using json path
     pages = jsonpath.jsonpath(json_object, 'total_pages')
@@ -21,6 +22,29 @@ def test_get_request():
     json_formatted_data = json.dumps(data, indent=2)
     print(json_formatted_data)
 
+    # validate the response using json path
+    assert jsonpath.jsonpath(json_object, 'pages') == 2
+    assert jsonpath.jsonpath(json_object, 'per_page') == 6
+    assert jsonpath.jsonpath(json_object, 'total') == 12
+    assert jsonpath.jsonpath(json_object, 'total_pages') == 2
 
 
+def test_get_single_user():
+    url = "https://reqres.in/api/users/2"
+    response = requests.get(url)
 
+    assert response.status_code == 200, "Response code is not 200. The response code is " + str(response.status_code)
+
+    # parse response to json
+    json_object = json.loads(response.content)
+    json_formatted_response = json.dumps(json_object, indent=2)
+    print(json_formatted_response)
+
+    # validate the response using json path
+    data = jsonpath.jsonpath(json_object, 'data')
+
+    assert data[0]['id'] == 2
+    assert data[0]['email'] == "janet.weaver@reqres.in"
+    assert data[0]['first_name'] == "Janet"
+    assert data[0]['last_name'] == "Weaver"
+    assert data[0]['avatar'] == "https://reqres.in/img/faces/2-image.jpg"
