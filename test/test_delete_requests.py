6154ab46cccd653dd60f681cc82bf815f514a0a7
absiddik7 <abubakkar1070@gmail.com>
2023-06-03 18:10:22 +0600

    first commit

---
+++ test/test_delete_requests.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def test_delete_user():
+    url = "https://reqres.in/api/users/2"
+    response = requests.delete(url)
+
+    # validate response code
+    assert response.status_code == 204, "Response code is not 204. The response code is " + str(response.status_code)
